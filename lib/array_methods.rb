49ec27b5a210e5e105b31b84eb4e2512c4d09476
John Odom <joodom91@gmail.com>
2019-06-15 13:26:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,9 @@
 def find_element_index(array, value_to_find)
   i=0
   while i<array.length do
-    # array.include? value_to_find
-    array.index(value_to_find)
     i+=1
   end
+  array.index(value_to_find)
 end
 
 
